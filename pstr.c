a195ab0092b3657f5f07f98c88246f35298b9203
Congo Justus Migue <congomigue@gmail.com>
2023-05-26 01:50:13 +0000

    #12: pstr Implement the pstr opcode.

---
+++ pstr.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+	(void)line_number;
+
+	current = *stack;
+
+	while (current != NULL && current->n != 0 && (current->n > 0 && current->n <= 127))
+	{
+		printf("%c", current->n);
+		current = current->next;
+	}
+	printf("\n");
+}
